b228b93ba6e475cbbce60764cb301fa865132a82
Sierra Baker <sierrabakes24@gmail.com>
2019-11-22 11:46:21 -0500

    CODED - login, register, forms for both small change #1

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,10 @@
-from app import app
 from datetime import datetime
 from flask import render_template, flash, redirect, url_for, request
 from flask_login import login_user, logout_user, current_user, login_required
 from werkzeug.urls import url_parse
 from app import app, db
 from app.forms import LoginForm, RegistrationForm
-from app.models import User, Product, Price, ProductToUser
+from app.models import User, Product, Price, ProductToUser, Category
 
 @app.before_request
 def before_request():
